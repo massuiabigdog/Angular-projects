8779fa4cf29dc86675e5c3b6dd79ab0b251db2c5
Tmassuia <thiago.massuia@hotmail.com>
2018-05-12 23:51:27 -0300

    lightbox

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,9 @@ import { HttpClient } from '@angular/common/http';
 
 import { NgModule } from '@angular/core';
 import { hammerjs} from 'hammerjs';
+
+declare var jquery:any;
+declare var $ :any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -12,48 +15,14 @@ import { hammerjs} from 'hammerjs';
   imports: [],
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit() {
-    // const sampleUrl = 'http://slowwly.robertomurray.co.uk/delay/6000/url/https://jsonplaceholder.typicode.com/posts/1';
-
-    // this.progressService.start();
-    // setTimeout(() => {
-    //   this.progressService.set(0.1);
-    // }, 1000);
-    // setTimeout(() => {
-    //   this.progressService.inc(0.2);
-    // }, 2000);
-    // this.http.get(sampleUrl)
-    //   .subscribe((response) => {
-    //     this.progressService.done();
-    //     this.posts = response.json();
-    //   });
+    $('button').click(function()
+  {
+    alert('hi');
+  })
   }
-
 }
-
-
-
-// export class AppComponent implements OnInit {
-
-//   projects = [];
-
-//   constructor(private http: HttpClient) {}
-
-//   ngOnInit(): void {
-//     this.http.get('http://localhost/projects/wordpress/wp-json/wp/v2/projects').subscribe( data => {
-//       for(let key in data){
-//         if(data.hasOwnProperty(key)){
-//           this.projects.push(data[key]);
-//         }
-//       }
-
-//       console.log(this.projects);
-//     })
-
-//   }
-// }
-
